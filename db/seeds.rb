9f25d9b70a275152e9d906adc947130991c59fe1
Maxime Chabrol <chabrolmx@gmail.com>
2019-11-19 12:14:32 +0100

    re-setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,31 @@ Product.destroy_all
 puts 'Creating 5 fake users...'
 
 User.create!{
-  email:  "toto@gmail.com",
+  email: "toto@gmail.com",
   password: "123456",
   first_name: "Toto",
   last_name: "Dujardin"
 }
 User.create!{
-  email:  "tonton@gmail.com",
+  email: "tonton@gmail.com",
   password: "123456",
   first_name: "Tonton",
   last_name: "Bob"
 }
 User.create!{
-  email:  "tata@gmail.com",
+  email: "tata@gmail.com",
   password: "123456",
   first_name: "Tata",
   last_name: "Georgette"
 }
 User.create!{
-  email:  "tutu@gmail.com",
+  email: "tutu@gmail.com",
   password: "123456",
   first_name: "Tutu",
   last_name: "Latutule"
 }
 User.create!{
-  email:  "titi@gmail.com",
+  email: "titi@gmail.com",
   password: "123456",
   first_name: "Titi",
   last_name: "Henry"
