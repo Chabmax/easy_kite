058f011abc949ed398a4021252c78176b2a1a1a3
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-19 14:16:31 +0100

    created autorisation with pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+
   include Pundit
 
   # Pundit: white-list approach.
