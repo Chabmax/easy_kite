6a03941e3d6a480ca3a500d1890747e0e4d92164
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-21 23:36:58 +0100

    Modify validation for Rental + Modify display booking in show

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -13,29 +13,36 @@ class Rental < ApplicationRecord
   def start_date_after_today?
     return if date_start.blank?
 
-    if date_start < Date.today
-      errors.add(:date_start, "must be equal or after today")
-    end
+    errors.add(:date_start, "must be equal or after today") if date_start < Date.today
   end
 
   def end_date_after_start_date?
     return if date_end.blank? || date_start.blank?
 
-    if date_end < date_start
-      errors.add(:date_end, "must be after the start date")
-    end
+    errors.add(:date_end, "must be after the start date") if date_end < date_start
   end
 
   def rental_period_is_free?
-    free = self.product.rentals.all? do |rental|
-      start_before_start = self.date_start < rental.date_start
-      end_before_start = self.date_end < rental.date_start
-      start_after_end = self.date_start > rental.date_end
-      end_after_end = self.date_end > rental.date_end
+    product.rentals.all? do |rental|
+      start_before_start = date_start < rental.date_start
+      end_before_start = date_end < rental.date_start
+      start_after_end = date_start > rental.date_end
+      end_after_end = date_end > rental.date_end
+
+      if date_start > rental.date_start && date_start < rental.date_end
+        errors.add(:date_start, "not available: alredy booked")
+      end
+
+      if date_end > rental.date_start && date_end < rental.date_end
+        errors.add(:date_end, "not available: alredy booked")
+      end
+
+      unless (start_before_start && end_before_start) || (start_after_end && end_after_end)
+        errors.add(:date_start, "not available: period not free")
+        errors.add(:date_end, "not available: period not free")
+      end
 
       (start_before_start && end_before_start) || (start_after_end && end_after_end)
     end
-
-    errors.add(:date_end, "date not available") unless free
   end
 end
