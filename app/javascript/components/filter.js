e7cebfa5e6ea84738822ad7f51a08d4721048223
Brian Ridolce <brianridolce@gmail.com>
2019-11-21 14:50:24 +0100

    refacto if du filter pour debug page product

--- app/javascript/components/filter.js
+++ app/javascript/components/filter.js
@@ -1,3 +1,5 @@
+const productCards = document.querySelectorAll('[data-category]');
+console.log(productCards)
 const hideCard = (productCards, categoryName) => {
   productCards.forEach(card => {
     // Lorsqu'on click, récupérer le nom de la catégorie
@@ -12,31 +14,33 @@ const hideCard = (productCards, categoryName) => {
   });
 }
 
-const changeButtonColor = (event) => {
-  event.currentTarget.classList.toggle('card-active')
+  const changeButtonColor = (event) => {
+    event.currentTarget.classList.toggle('card-active')
+  }
+
+  const handleClickOnCard = (event) => {
+    const categoryName = event.target.innerText;
+    changeButtonColor(event);
+    hideCard(productCards, categoryName);
+  }
+
+
+  const filter = () => {
+    const categoryElements = document.querySelectorAll('.card-category');
+    if (productCards) {
+    // Placer un micro sur chaque catégorie
+    categoryElements.forEach(element => {
+      element.addEventListener("click", handleClickOnCard);
+      })
+      const resetFilters = () => {
+      document.querySelector('.remove-filters').addEventListener('click', () => {
+      const productCards = document.querySelectorAll('[data-category]');
+      productCards.forEach(card => card.style.display = "block");
+      resetFilters();
+      })
+    }
+  }
 }
 
-const handleClickOnCard = (event) => {
-  const productCards = document.querySelectorAll('[data-category]');
-  const categoryName = event.target.innerText;
-  changeButtonColor(event);
-  hideCard(productCards, categoryName);
-}
-
-const resetFilters = () => {
-  document.querySelector('.remove-filters').addEventListener('click', () => {
-    const productCards = document.querySelectorAll('[data-category]');
-    productCards.forEach(card => card.style.display = "block");
-  })
-}
-
-const filter = () => {
-  const categoryElements = document.querySelectorAll('.card-category');
-  // Placer un micro sur chaque catégorie
-  categoryElements.forEach(element => {
-    element.addEventListener("click", handleClickOnCard);
-  })
-  resetFilters();
-}
 
 export default filter;
