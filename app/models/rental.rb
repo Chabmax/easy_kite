1da1289686c253f5138e644fc5778f258f9bf1ee
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-21 19:08:17 +0100

    refacto validation car silent

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,8 +3,10 @@ class Rental < ApplicationRecord
   belongs_to :product
 
   validates :date_start, :date_end, presence: true
+
   validate :start_date_after_today?
   validate :end_date_after_start_date?
+  validate :rental_period_is_free?
 
   private
 
@@ -22,5 +24,18 @@ class Rental < ApplicationRecord
     if date_end < date_start
       errors.add(:date_end, "must be after the start date")
     end
+  end
+
+  def rental_period_is_free?
+    free = self.product.rentals.all? do |rental|
+      start_before_start = self.date_start < rental.date_start
+      end_before_start = self.date_end < rental.date_start
+      start_after_end = self.date_start > rental.date_end
+      end_after_end = self.date_end > rental.date_end
+
+      (start_before_start && end_before_start) || (start_after_end && end_after_end)
+    end
+
+    errors.add(:date_end, "date not available") unless free
   end
 end
